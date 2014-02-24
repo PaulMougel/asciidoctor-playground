05e0e3822e0c40d5c07ec7880b014715f2465048
Paul Mougel <mougelpaul@gmail.com>
2014-02-24 19:51:30 +0100

    Added GET /document index

--- index.js
+++ index.js
@@ -2,19 +2,32 @@ var fs = require('fs');
 var path = require('path');
 var child_process = require('child_process');
 var express = require('express');
+var _ = require('underscore');
+
 var config = require('./config');
+var srcDir = path.join(config.document_directory, 'src');
+var renderedDir = path.join(config.document_directory, 'rendered');
 
 var app = express();
 
+app.get('/document', function (req, res) {
+	fs.readdir(renderedDir, function (err, files) {
+		if (err) return res.send(500, err.message);
+		res.send(_.filter(files, function (fileName) {
+			return fileName !== '.empty';
+		}));
+	})
+});
+
 app.get('/document/:document_name.adoc', function (req, res) {
-	res.sendfile(req.params.document_name + '.adoc', { root: path.join(config.document_directory, 'src') });
+	res.sendfile(req.params.document_name, { root: srcDir });
 });
 app.post('/document/:document_name.adoc', function (req, res) {
 	var asciidoctor = child_process.spawn('asciidoctor', ['-']);
-	req.pipe(fs.createWriteStream(path.join(config.document_directory, 'src', req.params.document_name + '.adoc')));
+	req.pipe(fs.createWriteStream(path.join(srcDir, req.params.document_name)));
 	req.pipe(asciidoctor.stdin);
 	
-	var output = fs.createWriteStream(path.join(config.document_directory, 'rendered', req.params.document_name + '.html'));
+	var output = fs.createWriteStream(path.join(renderedDir, req.params.document_name));
 	asciidoctor.stdout.pipe(output);
 	asciidoctor.stdout.on('end', function () {
 		res.send(200);
@@ -24,7 +37,8 @@ app.post('/document/:document_name.adoc', function (req, res) {
 	});
 });
 app.get('/document/:document_name.html', function (req, res) {
-	res.sendfile(req.params.document_name + '.html', { root: path.join(config.document_directory, 'rendered') });
+	res.set('Content-Type', 'text/html');
+	res.sendfile(req.params.document_name, { root: renderedDir });
 });
 
 app.listen(8080);
