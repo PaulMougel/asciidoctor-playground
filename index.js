95774fa0de0a52bf3e573dbc4644e668007b5f1b
Paul Mougel <mougelpaul@gmail.com>
2014-02-24 19:32:14 +0100

    Saves source Asciidoc file. Added route to GET it

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ var config = require('./config');
 
 var app = express();
 
-app.use('/document/', express.static(config.document_directory));
+app.use('/document/src/', express.static(config.document_directory + '/src'));
+app.use('/document/', express.static(config.document_directory + '/rendered'));
 app.post('/document/:document_name', function (req, res, next) {
 	var asciidoctor = child_process.spawn('asciidoctor', ['-']);
+	req.pipe(fs.createWriteStream(path.join(config.document_directory, 'src', req.params.document_name + '.adoc')));
 	req.pipe(asciidoctor.stdin);
 	
-	var output = fs.createWriteStream(path.join(config.document_directory, req.params.document_name + '.html'));
+	var output = fs.createWriteStream(path.join(config.document_directory, 'rendered', req.params.document_name + '.html'));
 	asciidoctor.stdout.pipe(output);
 	asciidoctor.stdout.on('end', function () {
 		res.send(200);
