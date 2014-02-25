d32f46463270aa7e3f1976de332e35ea1d30a273
Paul Mougel <mougelpaul@gmail.com>
2014-02-25 17:44:08 +0100

    Frontend draft

---
+++ public/documents.js
@@ -0,0 +1,24 @@
+angular.module('asciidoctorplayground', [])
+.controller('DocumentListController', function ($scope, $http) {
+	function reload() {
+		console.log('reload');
+		$http.get('/document')
+		.success(function (data) {
+			$scope.documents = data.documents;
+		});
+	}
+	reload();
+
+	$scope.upload = function (element) {
+		var file = element.files[0];
+		var formdata = new FormData();
+		formdata.append("file", file);
+		$http({
+			method: 'POST',
+			url: '/document/' + file.name,
+			data: formdata,
+			headers: { 'Content-Type': undefined },
+			transformRequest: function(data) { return data; }
+		}).success(reload);
+	}
+});
